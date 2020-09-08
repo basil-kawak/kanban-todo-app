6236f3cf9e3f3de1e67846d97008a3f4cf42c865
basil-kawak <58145973+basil-kawak@users.noreply.github.com>
2020-09-09 02:49:16 +0300

    Update db.js

--- src/containers/db.js
+++ src/containers/db.js
@@ -1,13 +1,13 @@
 import * as firebase from "firebase";
 var firebaseConfig = {
-  apiKey: "AIzaSyBC3HtzB2SS4BOiDNHQrvVW6maIy3TtOQU",
-  authDomain: "todo-3b08d.firebaseapp.com",
-  databaseURL: "https://todo-3b08d.firebaseio.com",
-  projectId: "todo-3b08d",
-  storageBucket: "todo-3b08d.appspot.com",
-  messagingSenderId: "148626405188",
-  appId: "1:148626405188:web:6622b8b8f755570920c911",
-  measurementId: "G-HGB26VWCDG"
+  apiKey: "AIzaSyCm_YxHHUGvO2wspIzdv8plaQ7PWtPS_Xw",
+  authDomain: "posts-8b494.firebaseapp.com",
+  databaseURL: "https://posts-8b494.firebaseio.com",
+  projectId: "posts-8b494",
+  storageBucket: "posts-8b494.appspot.com",
+  messagingSenderId: "1040292931183",
+  appId: "1:1040292931183:web:323db79b29f37113328f9a",
+  measurementId: "G-4BMPC5E3EC",
 };
 
 const fire = firebase.initializeApp(firebaseConfig);
