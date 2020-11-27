df774d73579d9c5557a9605830eda2c7e5eb4137
basil-kawak <basilalisa0@gmail.com>
2020-11-28 02:45:40 +0300

    Updates

---
+++ precache-manifest.841dcbf9562cfa9e777c389afd301764.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2d3c11e640657034501ea9f5177a67a",
+    "url": "/kanban-todo-app/index.html"
+  },
+  {
+    "revision": "f9d3c09d118622b1f78c",
+    "url": "/kanban-todo-app/static/css/main.d462f486.chunk.css"
+  },
+  {
+    "revision": "47d76e0982550291bd7c",
+    "url": "/kanban-todo-app/static/js/2.56b7e39b.chunk.js"
+  },
+  {
+    "revision": "a801b48d9f1292e198656e92a2514691",
+    "url": "/kanban-todo-app/static/js/2.56b7e39b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9d3c09d118622b1f78c",
+    "url": "/kanban-todo-app/static/js/main.66ce99ad.chunk.js"
+  },
+  {
+    "revision": "740e21021a65c3875865",
+    "url": "/kanban-todo-app/static/js/runtime-main.9db13b82.js"
+  }
+]);
